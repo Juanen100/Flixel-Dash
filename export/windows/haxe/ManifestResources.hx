4b5cb36ed628bb26372710e864f6033eff2dcc77
Juanen100 <65170591+Juanen100@users.noreply.github.com>
2023-08-13 20:11:44 +0200

    Stereo Madness
    Not implemented yet lol

--- export/windows/haxe/ManifestResources.hx
+++ export/windows/haxe/ManifestResources.hx
@@ -123,6 +123,7 @@ null
 @:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_modes_wave_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
 @:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_spikes_spike_01_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
 @:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_music_music_goes_here_txt extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_music_stereomadness_mp3 extends null { }
 @:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_sounds_sounds_go_here_txt extends null { }
 @:keep @:bind @:noCompletion #if display private #end class __ASSET__flixel_sounds_beep_ogg extends null { }
 @:keep @:bind @:noCompletion #if display private #end class __ASSET__flixel_sounds_flixel_ogg extends null { }
